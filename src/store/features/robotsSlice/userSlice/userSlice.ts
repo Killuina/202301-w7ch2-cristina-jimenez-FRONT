5b942eec449da14b3ed5fe9627d08b9c42da4b18
Cristina <cristinajmartin98@gmail.com>
2023-02-21 23:06:10 +0100

    Add userSlice with loginUser reducer case

---
+++ src/store/features/robotsSlice/userSlice/userSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User, UserState } from "./types";
+
+const initialState: UserState = {
+  username: "",
+  isLogged: false,
+  token: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginUser: (currentUserState, { payload }: PayloadAction<User>) => ({
+      ...payload,
+      isLogged: true,
+    }),
+  },
+});
+
+export const { reducer: userReducer } = userSlice;
+export const { loginUser: loginUserActionCreator } = userSlice.actions;
